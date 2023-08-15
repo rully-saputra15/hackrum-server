399b9ba042ba694001b4eb67441312f0f2829698
rully saputra <rully.saputra4@gmail.com>
2023-08-15 10:50:12 +0700

    feat: add rate limiter

---
+++ middlewares/rateLimiter.js
@@ -0,0 +1,18 @@
+const rateLimit = require("express-rate-limit");
+
+const loginLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 15 minutes
+  max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  // store: ... , // Use an external store for more precise rate limiting
+});
+
+const questionLimiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 15 minutes
+  max: 50, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+module.exports = { loginLimiter, questionLimiter };
--- routers/index.js
+++ routers/index.js
@@ -4,13 +4,13 @@ const UserController = require("../controllers/userController");
 const QuestionController = require("../controllers/questionController");
 const TypeController = require("../controllers/typeController");
 const authentication = require("../middlewares/authentication");
-
+const { loginLimiter, questionLimiter } = require("../middlewares/rateLimiter");
 const router = express.Router();
 
-router.post("/login", UserController.glogin);
+router.post("/login", loginLimiter, UserController.glogin);
 
 router.use(authentication);
-
+router.use(questionLimiter);
 router.get("/questions", QuestionController.getAllQuestion);
 router.get("/questions/:id", QuestionController.getQuestionById);
 router.post("/questions", QuestionController.createQuestion);
